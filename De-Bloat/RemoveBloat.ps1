07bb97db6202fb4c7b58a1b9db45d33d8ff832c9
sinn3D <63341707+kaempfde@users.noreply.github.com>
2025-01-21 13:00:15 +0100

    Update RemoveBloat.ps1
    Copilot wird nun ebenfalls entfernt

--- De-Bloat/RemoveBloat.ps1
+++ De-Bloat/RemoveBloat.ps1
@@ -374,6 +374,8 @@ $Bloatware = @(
     "C27EB4BA.DropboxOEM*"
     "*DevHome*"
     "*Outlook (new)*"
+    "Microsoft.Copilot"
+ 
     #Optional: Typically not removed but you can if you need to for some reason
     #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
     #"*Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe*"
